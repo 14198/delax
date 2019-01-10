697688b3bfc388608866442b46878d4e2e086197
Naoki-Y <axs2002est@gmail.com>
2019-01-10 10:27:15 +0900

    更新しました

--- Asan.java
+++ Asan.java
@@ -15,39 +15,9 @@ public class Asan extends Actor
     
     public void act() 
     {
-<<<<<<< HEAD
        
-        
-        if( Greenfoot.isKeyDown( "up" )){
-=======
-       if( Greenfoot.isKeyDown( "down" )){
-            setRotation(90);
-            move(4); // Add your action code here.
-        }
-           if(Greenfoot.isKeyDown("right")){
-           setRotation(0); 
-           move(4);
-        }
-
-        else if( Greenfoot.isKeyDown( "up" )){
->>>>>>> 149a8a233d6ad229e8e263379eda559176c1ed01
-            setRotation(270);
-            move(10);
-        }else if( Greenfoot.isKeyDown( "down" )){
-            setRotation(90);
-            move(10);
-        }
-<<<<<<< HEAD
-    } }   
-
-=======
-        if(Greenfoot.isKeyDown("left")){
-        setRotation(180);
-        move(4);
-    }
 
     }  
 
     }    
->>>>>>> 149a8a233d6ad229e8e263379eda559176c1ed01
 
