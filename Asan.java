10590c3eb596aab38eb5e5fca24739c7885065b1
14198 <3c18ty24@gmail.com>
2019-01-10 10:03:00 +0900

    更新1!

--- Asan.java
+++ Asan.java
@@ -12,28 +12,18 @@ public class Asan extends Actor
      * Act - do whatever the Asan wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
     public void act() 
     {
-       if( Greenfoot.isKeyDown( "down" )){
-            setRotation(90);
-            move(4); // Add your action code here.
-        }
-
-           if(Greenfoot.isKeyDown("right")){
-           setRotation(0); 
-           move(4);
-        }
-
-
-        else if( Greenfoot.isKeyDown( "up" )){
+       
+        
+        if( Greenfoot.isKeyDown( "up" )){
             setRotation(270);
             move(10);
+        }else if( Greenfoot.isKeyDown( "down" )){
+            setRotation(90);
+            move(10);
         }
-        // Add your action code here.
-        if(Greenfoot.isKeyDown("left")){
-        setRotation(180);
-        move(4);
-    }
-    }    
-}
+    } }   
+
 
